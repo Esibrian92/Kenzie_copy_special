2003c735b98f5fe99232d369c0b619cd20dce3b5
Esibrian92 <ErickSibrian1@gmail.com>
2021-04-07 11:38:40 -0400

    all_test_Pass

--- copyspecial.py
+++ copyspecial.py
@@ -39,6 +39,9 @@ def get_special_paths(dirname):
 
 
 def copy_to(path_list, dest_dir):
+    """checks to see if a directory exist, looks through it and copies the files
+     to a new given destination if it exist.if not it makes a given
+     destination"""
     if not os.path.isdir(dest_dir):
         os.makedirs(dest_dir)
     for path in path_list:
@@ -46,7 +49,7 @@ def copy_to(path_list, dest_dir):
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
+    """zips the files to a new destination"""
     cmd = ["zip", "-j", dest_zip]
     cmd.extend(path_list)
     subprocess.check_output(cmd)
@@ -59,6 +62,8 @@ def main(args):
         description="read,copy,and move directories")
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    """adds a new argument to the args.parse, so the from_dir command can be used
+     in the command line"""
     parser.add_argument('from_dir', help='files from directory')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
@@ -70,18 +75,26 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
+
+    """prints out the usage and exits the program with and exit code of 1 if ns
+     is untrue in anyway (errors)"""
     if not ns:
         parser.print_usage()
         sys.exit(1)
 
     # Your code here: Invoke (call) your functions
+    """by setting the funtion equal to a variable now we can use it as an object
+     and see everything it conatains"""
     special_paths = get_special_paths(ns.from_dir)
+
+    """conditions in wich to run the functions from above"""
 
     if ns.todir:
         copy_to(special_paths, ns.todir)
 
     elif ns.tozip:
-        print(f"I am going to : ")
+        message = "I am going to : "
+        print(message)
         zip_to(special_paths, ns.tozip)
     else:
         for path in special_paths:
